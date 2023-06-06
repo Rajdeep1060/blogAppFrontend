553a2c94726d8c179a325a28db112e072b656ce7
Rajdeep <rajdeep1060@chitkara.edu.in>
2023-06-06 09:37:30 +0530

    frontend commit

---
+++ src/pages/CreateBlog.js
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
+import toast from "react-hot-toast";
+
+const CreateBlog = () => {
+  const id = localStorage.getItem("userId");
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    title: "",
+    description: "",
+    image: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/v1/blog/create-blog", {
+        title: inputs.title,
+        description: inputs.description,
+        image: inputs.image,
+        user: id,
+      });
+      if (data?.success) {
+        toast.success("Blog Created");
+        navigate("/my-blogs");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Box
+          width={"50%"}
+          border={3}
+          borderRadius={10}
+          padding={3}
+          margin="auto"
+          bgcolor={"black"}
+          boxShadow={"10px 10px 20px #ccc"}
+          display="flex"
+          flexDirection={"column"}
+          marginTop="30px"
+        >
+          <Typography
+            variant="h2"
+            textAlign={"center"}
+            fontWeight="bold"
+            padding={3}
+            color="white"
+          >
+            Create A Post
+          </Typography>
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold", color: "white" }}
+          >
+            Title
+          </InputLabel>
+          <TextField
+            name="title"
+            value={inputs.title}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            style={{ color: "white", borderColor: "white" }}
+            InputProps={{
+              style: {
+                color: "white",
+              },
+              focus: {
+                borderBottomColor: "blue",
+              },
+            }}
+            required
+          />
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold", color: "white" }}
+          >
+            Description
+          </InputLabel>
+          <TextField
+            name="description"
+            value={inputs.description}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            style={{ color: "white", borderColor: "white" }}
+            InputProps={{
+              style: {
+                color: "white",
+              },
+              focus: {
+                borderBottomColor: "blue",
+              },
+            }}
+            required
+          />
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold", color: "white" }}
+          >
+            Image URL
+          </InputLabel>
+          <TextField
+            name="image"
+            value={inputs.image}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            style={{ color: "white", borderColor: "white" }}
+            InputProps={{
+              style: {
+                color: "white",
+              },
+              focus: {
+                borderBottomColor: "blue",
+              },
+            }}
+            required
+          />
+          <br />
+
+          <Button type="submit" color="primary" variant="contained">
+            SUBMIT
+          </Button>
+        </Box>
+      </form>
+    </>
+  );
+};
+
+export default CreateBlog;
